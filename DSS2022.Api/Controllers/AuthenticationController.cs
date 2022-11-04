317fe46de3f07e029fa1c6fd3ab545f315834937
Micaela Peralta <mika_peralta93@yahoo.com>
2022-11-04 00:31:43 -0300

    add user groupId to login response

--- DSS2022.Api/Controllers/AuthenticationController.cs
+++ DSS2022.Api/Controllers/AuthenticationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using DSS2022.Business;
 using DSS2022.Business.Helpers;
 using DSS2022.DataTransferObjects.User;
 using Newtonsoft.Json.Linq;
@@ -10,9 +11,16 @@ namespace DSS2022.Api.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
-            private CookieCollection collection;
-            string strCookietoPass;
-            string sessionID;
+        private CookieCollection collection;
+        private IBonitaBpmService _bonitaBpmService;
+
+        string strCookietoPass;
+        string sessionID;
+
+        public AuthenticationController(IBonitaBpmService bonitaBpmService)
+        {
+            _bonitaBpmService = bonitaBpmService;
+        }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserDTO userDTO)
@@ -21,11 +29,22 @@ namespace DSS2022.Api.Controllers
             var loginStr = await authenticationHelper.Login(userDTO);
             CookieOptions cookieOptions = new CookieOptions();
             cookieOptions.Domain = "localhost:3000";
-            if(loginStr.HasValues) {
-                HttpContext.Response.Cookies.Append("session-id", loginStr.GetValue("sessionId").ToString(), cookieOptions);
-                HttpContext.Response.Cookies.Append("api-token", loginStr.GetValue("apiToken").ToString(), cookieOptions);
-                JObject res = new JObject( new JProperty("credentials", loginStr));
-                return Ok(loginStr);
+            if (loginStr.HasValues)
+            {
+                string sessionId = loginStr.GetValue("sessionId").ToString();
+                string apiToken = loginStr.GetValue("apiToken").ToString();
+
+                string userId = await _bonitaBpmService.GetUserId(apiToken, sessionId, userDTO.Email);
+                string groupId = await _bonitaBpmService.GetGroupId(apiToken, sessionId, userId);
+
+                HttpContext.Response.Cookies.Append("session-id", sessionId, cookieOptions);
+                HttpContext.Response.Cookies.Append("api-token", apiToken, cookieOptions);
+                JObject res = new JObject(
+                    new JProperty("sessionId", sessionId),
+                    new JProperty("apiToken", apiToken),
+                    new JProperty("groupId", groupId)
+                );
+                return Ok(res);
             }
 
             return Unauthorized();
@@ -33,14 +52,12 @@ namespace DSS2022.Api.Controllers
 
         [HttpPost("logout")]
         public void Logout()
-            {
-                var bonitaSessionId = this.HttpContext.Request.Cookies["session-id"];
-                var bonitaApiKey = this.HttpContext.Request.Cookies["api-token"];
-                
-                AuthenticationHelper authenticationHelper = new AuthenticationHelper();
-                authenticationHelper.Logout(bonitaApiKey,bonitaSessionId);
-            }
+        {
+            var bonitaSessionId = this.HttpContext.Request.Cookies["session-id"];
+            var bonitaApiKey = this.HttpContext.Request.Cookies["api-token"];
 
-       
+            AuthenticationHelper authenticationHelper = new AuthenticationHelper();
+            authenticationHelper.Logout(bonitaApiKey, bonitaSessionId);
+        }
     }
 }
--- DSS2022.Business/IBonitaBpmService.cs
+++ DSS2022.Business/IBonitaBpmService.cs
@@ -7,5 +7,7 @@ public interface IBonitaBpmService
     Task<string> StartProcess(Collection collection, string id, string token, string sessionId);
     Task SetVariable(Collection collection, string token, string sessionId, string caseId);
     Task<string>  GetProcessId(string token, string sessionId);
+    Task<string>  GetUserId(string token, string sessionId, string userName);
+    Task<string>  GetGroupId(string token, string sessionId, string userId);
     Task<string> CreateCase(Collection collection, string processDefinitionId, string token, string sessionId);
 }
